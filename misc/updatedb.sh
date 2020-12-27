6b1f32a10066f533da0c7a22d333c18299e48671
Clément Bœsch <u@pkh.me>
2020-12-27 17:53:22 +0100

    misc: adjust updatedb.sh to manage both ladder and ragl

--- misc/updatedb.sh
+++ misc/updatedb.sh
@@ -2,5 +2,8 @@
 
 set -xeu
 
-~/venv/bin/ora-ladder /home/ora/server-data-*/support_dir/Replays/
-cp db.sqlite3 /home/web/venv/var/ladderweb-instance
+~/venv/bin/ora-ladder -d db-ladder.sqlite3 /home/ora/srv-ladder/instance-*/support_dir/Replays/
+~/venv/bin/ora-ragl   -d db-ragl.sqlite3   /home/ora/srv-ragl/instance-*/support_dir/Replays/
+
+cp db-ladder.sqlite3 /home/web/venv/var/ladderweb-instance/db.sqlite3
+cp db-ragl.sqlite3   /home/web/venv/var/raglweb-instance/db.sqlite3
