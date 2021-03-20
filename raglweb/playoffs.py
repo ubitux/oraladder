2f42a877a9fd8d6f4c230dd92275d4b9cd424228
Clément Bœsch <u@pkh.me>
2021-03-20 13:31:34 +0100

    ragl: rework playoff support
    The records are now consumed so that we can have the same players in
    several tiebreakers/playoffs.

--- raglweb/playoffs.py
+++ raglweb/playoffs.py
@@ -3,20 +3,38 @@ class PlayoffOutcome:
         self.profile_pair = (p0, p1)
 
 
-def records_count(records, outcome):
-    return len([r for r in records if outcome == r.profile_pair])
+def _extract_records(records, p0, p1, n):
+    win_score = n // 2 + 1
+
+    consumed = []
+    p0_wins, p1_wins = 0, 0
+
+    for record in records:
+        if record.profile_pair == (p0, p1):
+            p0_wins += 1
+            consumed.append(record)
+        elif record.profile_pair == (p1, p0):
+            p1_wins += 1
+            consumed.append(record)
+        else:
+            continue
+        if win_score in (p0_wins, p1_wins):
+            break
+
+    for record in consumed:
+        records.remove(record)  # will remove the first one only so this is fine
+
+    return p0_wins, p1_wins
 
 
 def get_playoff2(n, players, records):
     '''best of N with 2 players'''
 
+    records = list(records)
     p0, p1 = players
-    scores = {
-        p0: records_count(records, (p0, p1)),
-        p1: records_count(records, (p1, p0)),
-    }
+    p0_wins, p1_wins = _extract_records(records, p0, p1, n)
     return [
-        ('TieBreaker', (None, None), (p0, p1), (scores[p0], scores[p1])),
+        ('TieBreaker', (None, None), (p0, p1), (p0_wins, p1_wins)),
     ]
 
 
@@ -26,48 +44,43 @@ _GOLD, _SILVER, _BRONZE = '🥇', '🥈', '🥉'
 def get_playoff4(n, players, records):
     '''best of N with 4 players'''
 
+    records = list(records)
     p0, p1, p2, p3 = players
 
-    semi_scores = {
-        p0: records_count(records, (p0, p3)),
-        p3: records_count(records, (p3, p0)),
-        p1: records_count(records, (p1, p2)),
-        p2: records_count(records, (p2, p1)),
-    }
+    p0_wins, p3_wins = _extract_records(records, p0, p3, n)
+    p1_wins, p2_wins = _extract_records(records, p1, p2, n)
+    semi_wins = [p0_wins, p1_wins, p2_wins, p3_wins]
+    semi_scores = dict(zip(players, semi_wins))
 
     matchups = [
-        ('Semi-finals', (None, None), (p0, p3), (semi_scores[p0], semi_scores[p3])),
-        ('Semi-finals', (None, None), (p1, p2), (semi_scores[p1], semi_scores[p2])),
+        ('Semi-finals', (None, None), (p0, p3), (p0_wins, p3_wins)),
+        ('Semi-finals', (None, None), (p1, p2), (p1_wins, p2_wins)),
     ]
 
     win_score = n // 2 + 1
-    if list(semi_scores.values()).count(win_score) != 2:
+    if semi_wins.count(win_score) != 2:
         return matchups
 
     fp0, fp1 = fp = {p for p, score in semi_scores.items() if score == win_score}
     bp0, bp1 = semi_scores.keys() - fp
 
     # Bronze
-    bronze_scores = {
-        bp0: records_count(records, (bp0, bp1)),
-        bp1: records_count(records, (bp1, bp0)),
-    }
-    if win_score in bronze_scores.values():
-        bronze_status = (_BRONZE, None) if bronze_scores[bp0] == win_score else (None, _BRONZE)
+    bp0_wins, bp1_wins = _extract_records(records, bp0, bp1, n)
+    bronze_scores = {bp0: bp0_wins, bp1: bp1_wins}
+    if win_score in {bp0_wins, bp1_wins}:
+        bronze_status = (_BRONZE, None) if bp0_wins == win_score else (None, _BRONZE)
     else:
         bronze_status = (None, None)
-    matchups.append(('3rd Place', bronze_status, (bp0, bp1), (bronze_scores[bp0], bronze_scores[bp1])))
+    matchups.append(('3rd Place', bronze_status, (bp0, bp1), (bp0_wins, bp1_wins)))
 
     # Finale
-    finale_scores = {
-        fp0: records_count(records, (fp0, fp1)),
-        fp1: records_count(records, (fp1, fp0)),
-    }
-    if win_score in finale_scores.values():
-        finale_status = (_GOLD, _SILVER) if finale_scores[fp0] == win_score else (_SILVER, _GOLD)
+    fp0_wins, fp1_wins = _extract_records(records, fp0, fp1, n)
+    finale_scores = {fp0: fp0_wins, fp1: fp1_wins}
+    if win_score in {fp0_wins, fp1_wins}:
+        finale_status = (_GOLD, _SILVER) if fp0 == win_score else (_SILVER, _GOLD)
     else:
         finale_status = (None, None)
-    matchups.append(('Final', finale_status, (fp0, fp1), (finale_scores[fp0], finale_scores[fp1])))
+    matchups.append(('Final', finale_status, (fp0, fp1), (fp0_wins, fp1_wins)))
 
     return matchups
 
@@ -78,17 +91,23 @@ def _run():
     mrk = 'Morkel'
     ilm = 'I Like Men'
 
-    players = [god, mrk, ilm, zxg]
+    players2 = [ilm, zxg]
+    players4 = [god, mrk, ilm, zxg]
 
     T = PlayoffOutcome
     records = (
+        # fake tie breaker
+        T(zxg, ilm), T(ilm, zxg), T(ilm, zxg),
+
+        # finale
         T(god, zxg), T(god, zxg), T(god, zxg),
         T(zxg, ilm), T(ilm, zxg), T(ilm, zxg), T(zxg, ilm), T(ilm, zxg),
         T(god, mrk), T(mrk, god), T(mrk, god), T(god, mrk), T(god, mrk),
         T(ilm, mrk), T(mrk, ilm), T(mrk, ilm), T(ilm, mrk), T(mrk, ilm),
     )
 
-    print(get_playoff4(5, players, records))
+    print(get_playoff2(3, players2, records))
+    print(get_playoff4(5, players4, records))
 
 
 if __name__ == '__main__':
