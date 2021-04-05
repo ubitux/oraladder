4c23804ed61b350de1427a4e077e48a2b5224a11
Clément Bœsch <u@pkh.me>
2021-04-05 13:00:49 +0200

    misc/updatedb: make copy verbose

--- misc/updatedb.sh
+++ misc/updatedb.sh
@@ -8,5 +8,5 @@ set -xeu
 ~/venv/bin/ora-ladder -d db-td-2m.sqlite3 -p 2m /home/ora/srv-ladder-td/instance-*/support_dir/Replays/
 ~/venv/bin/ora-ragl   -d db-ragl.sqlite3   /home/ora/srv-ragl/instance-*/support_dir/Replays/
 
-cp db-ragl.sqlite3 /home/web/venv/var/raglweb-instance/
-cp db-*-*.sqlite3  /home/web/venv/var/ladderweb-instance/
+cp -v db-ragl.sqlite3 /home/web/venv/var/raglweb-instance/
+cp -v db-*-*.sqlite3  /home/web/venv/var/ladderweb-instance/
