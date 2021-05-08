5f102f49a2e3f6fdee52d7143a79e945e1ab97c4
Clément Bœsch <u@pkh.me>
2021-05-08 12:29:10 +0200

    misc/updatedb: use --bans-file on ladder

--- misc/updatedb.sh
+++ misc/updatedb.sh
@@ -2,10 +2,10 @@
 
 set -xeu
 
-~/venv/bin/ora-ladder -d db-ra-all.sqlite3      /home/ora/srv-ladder/instance-*/support_dir/Replays/
-~/venv/bin/ora-ladder -d db-ra-2m.sqlite3 -p 2m /home/ora/srv-ladder/instance-*/support_dir/Replays/
-~/venv/bin/ora-ladder -d db-td-all.sqlite3      /home/ora/srv-ladder-td/instance-*/support_dir/Replays/
-~/venv/bin/ora-ladder -d db-td-2m.sqlite3 -p 2m /home/ora/srv-ladder-td/instance-*/support_dir/Replays/
+~/venv/bin/ora-ladder --bans-file /home/ora/bans.list -d db-ra-all.sqlite3      /home/ora/srv-ladder/instance-*/support_dir/Replays/
+~/venv/bin/ora-ladder --bans-file /home/ora/bans.list -d db-ra-2m.sqlite3 -p 2m /home/ora/srv-ladder/instance-*/support_dir/Replays/
+~/venv/bin/ora-ladder --bans-file /home/ora/bans.list -d db-td-all.sqlite3      /home/ora/srv-ladder-td/instance-*/support_dir/Replays/
+~/venv/bin/ora-ladder --bans-file /home/ora/bans.list -d db-td-2m.sqlite3 -p 2m /home/ora/srv-ladder-td/instance-*/support_dir/Replays/
 ~/venv/bin/ora-ragl   -d db-ragl.sqlite3   /home/ora/srv-ragl/instance-*/support_dir/Replays/
 
 cp -v db-ragl.sqlite3 /home/web/venv/var/raglweb-instance/
