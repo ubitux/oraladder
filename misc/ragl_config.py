f1caac4a41b4516e8266d4d451e2b9385121c0c3
tttppp <tttppp@users.noreply.github.com>
2021-10-16 09:32:28 +0100

    Info page simplification.

--- misc/ragl_config.py
+++ misc/ragl_config.py
@@ -2,15 +2,18 @@ from datetime import date, timedelta
 
 SEASON = 11
 START_TIME = date(2021, 10, 11)
+GROUP_STAGE_WEEKS = 7
+PLAYOFF_WEEKS = 3
+DURATION_WEEKS = GROUP_STAGE_WEEKS + PLAYOFF_WEEKS
+END_TIME = START_TIME + timedelta(weeks=DURATION_WEEKS)
 MAP_PACK_VERSION = '2021-10-16'
 RELEASE = 'release-20210321'
 RELEASE_URL = 'https://github.com/OpenRA/OpenRA/releases/tag/release-20210321'
 SCHEDULE_URL = 'https://forum.openra.net/viewtopic.php?f=85&t=21511'
+RULES_URL = 'https://forum.openra.net/viewtopic.php?f=85&t=21492'
 PRIZE_POOL = '$1600'
-STAFF = ('.won (.1)', 'Happy', 'Anjew')
 DISCORD_URL = 'https://discord.gg/99zBDuS'
 DISCORD_NAME = 'Red Alert Competitive Discord'
-CONTACT_MAIL = 'OpenRA.RAGL@gmail.com'
 
 # We need <matchup_count> done every <matchup_delay> (one matchup
 # represents 2 games between the same players)
