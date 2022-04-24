8b318a31ffd32f3776a1ba67c918f9e8779c2fb2
Markus Suhr <markus@msuhr.net>
2022-04-24 18:29:26 +0200

    Added configs and info for RAGL S12

--- laddertools/ragl.py
+++ laddertools/ragl.py
@@ -227,7 +227,7 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--database', default='db-ragl.sqlite3')
     parser.add_argument('-s', '--schema', default=op.join(op.dirname(__file__), 'ragl.sql'))
-    parser.add_argument('-p', '--playersinfo', default=op.join(op.dirname(__file__), 'ragl-s11.yml'))
+    parser.add_argument('-p', '--playersinfo', default=op.join(op.dirname(__file__), 'ragl-s12.yml'))
     parser.add_argument('replays', nargs='*')
     args = parser.parse_args()
 
