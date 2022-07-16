8eafc08827fd3d6f1909dbe9daf2cd3874917580
Markus Suhr <markus@msuhr.net>
2022-07-16 10:55:34 +0200

    ragl: Added RAGL season 12 final configuration (players, map pack, forfeits, playoffs)

--- misc/ragl_config.py
+++ misc/ragl_config.py
@@ -6,12 +6,12 @@ GROUP_STAGE_WEEKS = 7
 PLAYOFF_WEEKS = 3
 DURATION_WEEKS = GROUP_STAGE_WEEKS + PLAYOFF_WEEKS
 END_TIME = START_TIME + timedelta(weeks=DURATION_WEEKS)
-MAP_PACK_VERSION = '2022-04-20'
+MAP_PACK_VERSION = '2022-06-05'
 RELEASE = 'release-20210321'
 RELEASE_URL = 'https://github.com/OpenRA/OpenRA/releases/tag/release-20210321'
 SCHEDULE_URL = 'https://forum.openra.net/viewtopic.php?f=85&t=21599'
 RULES_URL = 'https://forum.openra.net/viewtopic.php?f=85&t=21590'
-PRIZE_POOL = '$???'
+PRIZE_POOL = '$180'
 DISCORD_URL = 'https://discord.gg/99zBDuS'
 DISCORD_NAME = 'Red Alert Competitive Discord'
 GAMES_PER_MATCH = 2
